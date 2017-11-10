c24384eb8f780ec393dba899babee92002c8397b
Seth Vargo <seth@sethvargo.com>
2017-11-10 13:28:28 -0500

    Awk is sad

--- version/version.go
+++ version/version.go
@@ -2,16 +2,11 @@ package version
 
 import "fmt"
 
-// Version is the version of the project.
 const Version = "0.0.1"
 
 var (
-	// Name is the name of the project.
-	Name string
-
-	// GitCommit is the git sha of this build.
+	Name      string
 	GitCommit string
 
-	// HumanVersion is the human-friendly version.
 	HumanVersion = fmt.Sprintf("%s v%s (%s)", Name, Version, GitCommit)
 )
