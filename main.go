9fabac6608a5f526897521721644f4004edced57
Seth Vargo <seth@sethvargo.com>
2021-12-29 14:33:46 -0500

    Recover from panics and log a message (#36)

--- main.go
+++ main.go
@@ -1,28 +1,40 @@
 package main
 
 import (
-	"log"
 	"os"
 
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/sdk/plugin"
 	"github.com/sethvargo/vault-secrets-gen/gen"
 )
 
 func main() {
-	apiClientMeta := &api.PluginAPIClientMeta{}
-	flags := apiClientMeta.FlagSet()
+	logger := hclog.New(&hclog.LoggerOptions{})
+
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Error("plugin paniced", "error", r)
+			os.Exit(1)
+		}
+	}()
+
+	meta := &api.PluginAPIClientMeta{}
+
+	flags := meta.FlagSet()
 	if err := flags.Parse(os.Args[1:]); err != nil {
-		log.Fatal(err)
+		logger.Error("failed to parse flags: %s", err)
+		os.Exit(1)
 	}
 
-	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsConfig := meta.GetTLSConfig()
 	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
 	if err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: gen.Factory,
 		TLSProviderFunc:    tlsProviderFunc,
 	}); err != nil {
-		log.Fatal(err)
+		logger.Error("plugin shutting down", "error", err)
+		os.Exit(1)
 	}
 }
